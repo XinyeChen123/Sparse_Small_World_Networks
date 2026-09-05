f37c231cb326d51b172e39c455d904850a5706fd
XinyeChen123 <xinyec4@illinois.edu>
2026-09-04 19:36:25 -0500

    Add files via upload

--- ER_graph_simulation.py
+++ ER_graph_simulation.py
@@ -1,64 +1,90 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import networkx as nx
-from networkx import from_numpy_array
+from networkx import from_numpy_array, predecessor
 
-# parameters
-n = 100
-l = n/2
-alpha = 2
-p = alpha/n
-# random graph
-r = np.random.default_rng(seed=123)
-A = np.zeros((n, n), dtype=int)
+import Dijkstras_Algorithm as Dijkstras
 
-for  i in range(n):
-    j = (i + 1) % n
-    A[i, j] = 1
-    A[j, i] = 1
+def main():
+    # parameters
+    n = 100
+    l = n/2
+    alpha = 2
+    p = alpha/n
+    # random graph
+    r = np.random.default_rng(seed=123)
+    A = np.zeros((n, n), dtype=int)
 
-# generating shortcuts
+    for  i in range(n):
+        j = (i + 1) % n
+        A[i, j] = 1
+        A[j, i] = 1
 
-shortcuts = []
+    # generating shortcuts
 
-for i in range(n):
-    for j in range(i+1, n): # j > i
-        distance = min((j-i), n - (j-i))
+    shortcuts = []
 
-        if (2 <= distance <= l):
-            if r.random() < p:
-                A[i, j] = 1
-                A[j, i] = 1
-                shortcuts.append((i, j))
+    for i in range(n):
+        for j in range(i+1, n): # j > i
+            distance = min((j-i), n - (j-i))
 
-print("Number of vertices:", n)
-print("Shortcut probability:", p)
-print("Number of shortcut edges:", len(shortcuts))
-print("Shortcut edges:", shortcuts)
-print("Adjacency matrix:")
-print(A)
+            if (2 <= distance <= l):
+                if r.random() < p:
+                    A[i, j] = 1
+                    A[j, i] = 1
+                    shortcuts.append((i, j))
 
-# drawing graph with network
-G = from_numpy_array(A)
+    print("Number of vertices:", n)
+    print("Shortcut probability:", p)
+    print("Number of shortcut edges:", len(shortcuts))
+    print("Shortcut edges:", shortcuts)
+    print("Adjacency matrix:")
+    print(A)
 
-pos = nx.circular_layout(G)
+    # drawing graph with network
+    G = from_numpy_array(A)
 
-cycle_edges = []
-for i in range(n):
-    cycle_edges.append((i, (i+1)%n))
+    pos = nx.circular_layout(G)
+
+    cycle_edges = []
+    for i in range(n):
+        cycle_edges.append((i, (i+1)%n))
 
 
-plt.figure(figsize=(10, 10))
+    plt.figure(figsize=(10, 10))
 
-nx.draw_networkx_nodes(G,pos,node_size=40)
+    nx.draw_networkx_nodes(G,pos,node_size=40)
 
-# original edges
-nx.draw_networkx_edges(G,pos,edgelist=cycle_edges,edge_color="steelblue",width=1.5)
-# shortcut edges
-nx.draw_networkx_edges(G,pos,edgelist=shortcuts,edge_color="red",style="dashed",width=1,alpha=0.6)
+    # original edges
+    nx.draw_networkx_edges(G,pos,edgelist=cycle_edges,edge_color="steelblue",width=1.5)
+    # shortcut edges
+    nx.draw_networkx_edges(G,pos,edgelist=shortcuts,edge_color="red",style="dashed",width=1,alpha=0.6)
 
-plt.title(
-    f"Small-World Network: n={n}, ℓ={l}, α={alpha}"
-)
-plt.axis("off")
-plt.show()
+
+
+    graph_vertices = []
+    for i in range(n):
+        graph_vertices.append(i)
+
+
+
+
+    distances, predecessors = Dijkstras.dijkstra(A, graph_vertices, n)
+
+    predecessors.pop(0)
+
+    edges = []
+    for i in range(len(predecessors)-1):
+        edges.append((predecessors[i], predecessors[i+1]))
+
+    #print(edges)
+    print(predecessors)
+    #nx.draw_networkx_edges(G, pos, edgelist=edges, edge_color="green", width=1, alpha=0.6)
+    plt.title(
+        f"Small-World Network: n={n}, ℓ={l}, α={alpha}"
+    )
+    plt.axis("off")
+    plt.show()
+
+if __name__ == "__main__":
+    main()
